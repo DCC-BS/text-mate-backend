07e058a1a3522b5588cc44458e869552d1980a50
Tobias Bollinger <tobias.bollinger@gmail.com>
2025-04-03 10:15:09 +0200

    Update .env.example and configuration handling; modify API ports and remove development flag

--- src/text-mate-backend/utils/configuration.py
+++ src/text-mate-backend/utils/configuration.py
@@ -3,7 +3,6 @@ import os
 
 class Configuration:
     def __init__(self) -> None:
-        self.isDev: bool = os.getenv("ENV", "dev") == "true"
         self.api_port: int = int(os.getenv("API_PORT", 8090))
         self.openai_api_base_url: str = os.getenv("OPENAI_API_BASE_URL", "http://localhost:8000/v1")
         self.openai_api_key: str = os.getenv("OPENAI_API_KEY", "none")
@@ -14,7 +13,6 @@ class Configuration:
     def __str__(self) -> str:
         return f"""
         Configuration(
-            isDev={self.isDev},
             api_port={self.api_port},
             client_url={self.client_url},
             openai_api_base_url={self.openai_api_base_url},
