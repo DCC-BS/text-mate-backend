6a2499a061f4a8770999523040791b0d9f1fb0f4
Schraner, Yanick <yanick.schraner@bs.ch>
2025-04-04 16:59:41 +0200

    Update configuration.py

--- src/text-mate-backend/utils/configuration.py
+++ src/text-mate-backend/utils/configuration.py
@@ -3,7 +3,7 @@ import os
 
 class Configuration:
     def __init__(self) -> None:
-        self.api_port: int = int(os.getenv("API_PORT", 8090))
+        self.port: int = int(os.getenv("PORT", 8090))
         self.openai_api_base_url: str = os.getenv("OPENAI_API_BASE_URL", "http://localhost:8000/v1")
         self.openai_api_key: str = os.getenv("OPENAI_API_KEY", "none")
         self.llm_model: str = os.getenv("LLM_MODEL", "ollama_chat/llama3.2")
@@ -13,7 +13,7 @@ class Configuration:
     def __str__(self) -> str:
         return f"""
         Configuration(
-            api_port={self.api_port},
+            port={self.port},
             client_url={self.client_url},
             openai_api_base_url={self.openai_api_base_url},
             openai_api_key={self.openai_api_key},
