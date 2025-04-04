fca942b02761100499d86cfb57dbd1512f22e4ee
Schraner, Yanick <yanick.schraner@bs.ch>
2025-04-04 16:14:26 +0200

    use path from env

--- docker/languagetool/download_ngrams.sh
+++ docker/languagetool/download_ngrams.sh
@@ -5,10 +5,10 @@ download_ngrams() {
     lang=$1
     base_url="https://languagetool.org/download/ngram-data"
 
-    if [ ! -d "/ngrams/$lang" ] || [ -z "$(ls -A /ngrams/$lang)" ]; then
+    if [ ! -d "$langtool_languageModel/$lang" ] || [ -z "$(ls -A $langtool_languageModel/$lang)" ]; then
         # create the directory if it doesn't exist
         # mkdir -p "/ngrams"
-        
+
         echo "Searching for the latest n-grams for $lang..."
 
         # Get the list of available files and sort by date (newest first)
@@ -26,7 +26,7 @@ download_ngrams() {
         wget -e dotbytes=100M "$base_url/$latest_file" -O "/tmp/$latest_file"
 
         echo "Extracting n-grams..."
-        7z x "/tmp/$latest_file" -o"/ngrams/"
+        7z x "/tmp/$latest_file" -o"$langtool_languageModel/"
         rm "/tmp/$latest_file"
 
         echo "N-grams for $lang downloaded and extracted."
