160e6c187d6a192d7d27dea285a87171cea56cd5
Tobias Bollinger <tobias.bollinger@bs.ch>
2025-04-09 13:59:27 +0200

    fixed the language tool docker

--- docker/languagetool/entrypoint.sh
+++ docker/languagetool/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
 
+echo "Running entrypoint as $(whoami)"
+
 source /usr/local/bin/download_ngrams.sh &&
 download_ngrams de
 download_ngrams en
 download_ngrams fr
 
-source /LanguageTool/start.sh
+chmod +x /LanguageTool/start.sh
+
+echo "Starting LanguageTool as languagetool user ..."
+su-exec languagetool /LanguageTool/start.sh
